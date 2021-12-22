3cf7b50bdae2b1051809baa26ac32273f3ab1dd8
Ilhan Yapici <ilhan-yapici@outlook.fr>
2021-12-22 21:12:15 +0100

    template file

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const securityRouter = require("./routes/security");
+const userRouter = require("./routes/user");
+const articleRouter = require("./routes/article");
+const commentRouter = require("./routes/comment");
+const verifyWebToken = require("./middleware/verifyWebToken");
 const app = express();
 
-const connexion = require("./lib/db");
-connexion.sync();
-
-
-// - - - - - //
-// Code here //
-// - - - - - //
+const connection = require("./lib/db");
+connection.sync();
 
 app.use(express.json());
-app.listen(3000, () => console.log("Server is listening."))
+
+// Le router 'comment' est intégré dans le router 'articleRouter'
+// Pour lister tous les commentaires de tous les articles, il faut GET sur /articles/comments
+// Pour lister tous les commentaires d'un article, il faut GET sur /articles/:id/comments
+// Pour lister un commentaire précis, il faut GET sur /articles/comments/:id
+
+// Un commentaire est forcément lié à un article, donc pour poster un commentaire
+// il faut POST sur /articles/:id/comments
+
+app.use("", securityRouter);
+app.use("/users", userRouter);
+app.use("/articles", verifyWebToken, articleRouter);
+app.use("/comments", verifyWebToken, commentRouter);
+
+app.listen(3000, () => console.log("Server is listening."));
